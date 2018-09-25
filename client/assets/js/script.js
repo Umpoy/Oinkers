ef5d086857bf88ec9bef3d600de3ccb704b99ac9
Umpoy <iumpoy@yahoo.com>
2018-09-25 13:47:57 -0700

    loading GIF appears after sending data from client

--- client/assets/js/script.js
+++ client/assets/js/script.js
@@ -1,5 +1,8 @@
 
 const form = document.querySelector('.form');
+const loadingElement = document.querySelector('.loading');
+
+loadingElement.style.display = 'none';
 
 form.addEventListener('submit', (event) => {
     event.preventDefault(); //Prevent page refresh on form submit
@@ -7,4 +10,7 @@ form.addEventListener('submit', (event) => {
     const name = formData.get('name');
     const content = formData.get('content');
     console.log({ name, content });
+
+    form.style.display = 'none';
+    loadingElement.style.display = '';
 })
